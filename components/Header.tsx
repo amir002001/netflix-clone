68d2c9d0c3b8abcda659a0e89179bbe50926347b
Amir Afshari <a_amirhossein@icloud.com>
2022-10-19 18:17:31 -0400

    adds state to header for fixed behaviour

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,29 @@
 /* eslint-disable @next/next/no-img-element */
 import { BellIcon, MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header>
+    <header className={`${isScrolled && "bg-[#141414]"}`}>
       <div className="flex items-center space-x-2 md:space-x-10">
         <img
           alt="logo"
