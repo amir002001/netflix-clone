86912f6ba45a153f9983dfd5cbba00e7eddf799c
Amir Afshari <a_amirhossein@icloud.com>
2022-10-19 17:33:28 -0400

    adds ul with spacing

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <header>
+      <div className="flex items-center space-x-2 md:space-x-10">
+        <img
+          alt="logo"
+          src="/Netflix_2015_logo.svg"
+          width={100}
+          height={100}
+          className="cursor-pointer object-contain"
+        />
+        <ul className="hidden space-x-4 md:flex">
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li>Movies</li>
+          <li>New & Popular</li>
+          <li>My List</li>
+        </ul>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
