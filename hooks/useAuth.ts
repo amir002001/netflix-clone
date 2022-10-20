9a8a38ff772f519e3e35ea8af5df5b49984aa887
Amir Afshari <a_amirhossein@icloud.com>
2022-10-20 10:23:49 -0400

    adds react hook forms and useAuth hook

---
+++ hooks/useAuth.ts
@@ -0,0 +1,28 @@
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  User,
+} from 'firebase/auth'
+
+import { useRouter } from 'next/router'
+import { createContext, useContext, useEffect, useMemo, useState } from 'react'
+import { auth } from '../firebase'
+
+interface IAuth {
+  user: User | null
+  signUp: (email: string, password: string) => Promise<void>
+  signIn: (email: string, password: string) => Promise<void>
+  logout: () => Promise<void>
+  error: string | null
+  loading: boolean
+}
+
+function useAuth() {
+  const [user, setUser] = useState();
+
+  return <div>useAuth</div>;
+}
+
+export default useAuth;
